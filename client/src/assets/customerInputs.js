3be3945aaa3c12cdf5d8151582ad33cb976c6eb6
CurtiePi <curtisbklyn@gmail.com>
2020-11-04 12:59:58 -0500

    styling customer form pages and created a form page for ports

--- client/src/assets/customerInputs.js
+++ client/src/assets/customerInputs.js
@@ -2,44 +2,51 @@ export default {
   inputFields: [
     {
       name: 'fname',
+      label: 'First Name',
       type: 'text',
       placeholder: 'First Name',
       value: null
     },
     {
       name: 'lname',
+      label: 'Last Name',
       type: 'text',
       placeholder: 'Last Name',
       value: null
     },
     {
       name: 'address',
+      label: 'Address',
       type: 'text',
       placeholder: 'Address',
       value: null
     },
     {
       name: 'email',
+      label: 'Email',
       type: 'text',
       placeholder: 'Email',
       value: null
     },
     {
       name: 'phone',
+      label: 'Phone',
       type: 'text',
       placeholder: 'Phone',
       value: null
     },
     {
       name: 'boat_name',
+      label: 'Boat Name',
       type: 'text',
       placeholder: 'Boat Name',
       value: null
     },
     {
       name: 'boat_home',
-      type: 'text',
-      placeholder: 'Boat Home',
+      label: 'Boat Port',
+      type: 'select',
+      placeholder: 'Boat Port',
       value: null
     }
   ]
