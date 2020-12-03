c180dd42104c026377a06112af5428be1deff5a8
CurtiePi <curtisbklyn@gmail.com>
2020-12-02 22:10:25 -0500

    removed link from splash page

--- client/src/components/Hello.vue
+++ client/src/components/Hello.vue
@@ -2,9 +2,6 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <h2 :sp="salesperson">Welcome {{ salesperson.fname }} </h2>
-    <ul>
-      <li><router-link to="/customers">Customers</router-link></li>
-    </ul>
   </div>
 </template>
 
