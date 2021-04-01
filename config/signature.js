34fe69f82713744857cbf4b28d6ab78ec4705b6b
CurtiePi <curtisbklyn@gmail.com>
2021-04-01 17:04:14 -0400

    modified the signature font, font-size, and spacing

--- config/signature.js
+++ config/signature.js
@@ -1,11 +1,11 @@
 
 const glyph = '~~_/)~~';
-const name = '<p style="font-family: Ariel; color: #436663; font-size: 2em">David Martin</p>';
+const name = '<br/><span style="font-family: Arial; color: #436663; font-size: 1.75em">Capt. David Martin</span>';
 const email = '<a href="mailto:dave@uksailmakers-ne.com">dave@uksailmakers-ne.com</a>';
 const phone = '646-48305898';
 const site = '<a href="www.uksailmakers-ne.com">www.uksailmakers-ne.com</a>';
 const embedded = '<img src="cid:dave@uk-sailmakers-ne.com"/>'
 
-signature = `${glyph}${name}<br/>${email}<br/>${phone}<br/>${site}<br/>${embedded}`;
+signature = `${glyph}${name}<br/>${email}<br/>${phone}<br/>${site}i<br/><br/>${embedded}`;
 
 module.exports = signature;
