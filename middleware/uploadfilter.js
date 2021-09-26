d19eeafc80296f190978593b441af707188c3c30
CurtiePi <curtisbklyn@gmail.com>
2021-09-25 23:09:08 -0400

    Use filter to locate file path of attachment files

--- middleware/uploadfilter.js
+++ middleware/uploadfilter.js
@@ -1,5 +1,6 @@
 const fs = require('fs'); 
-
+const path = require('path'); 
+const fsPromises = fs.promises;
 
 const checkUpload = (err, req, res, next) => {
     if (err.code === "INCORRECT_FILETYPE") {
@@ -21,7 +22,38 @@ const renameUpload = (req, res, next) => {
     })
 }
 
+const findPath = async (req, res, next) => {
+    var directory = 'public/files';
+
+    const search = async function (directory) {
+        let directories;
+        try {
+            directories = await fsPromises.readdir(directory);
+            return directories;
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+
+    var paths = await search(directory);
+    paths = paths.filter(d => d !== '.DS_Store');
+
+    for (idx in paths) {
+        var dir_path = `${directory}/${paths[idx]}`;
+        directories = await search(dir_path);
+        var path = directories.filter(f => f === req.body.attachment);
+        if (path.length !== 0) {
+            req.attachment_path = `${dir_path}`;
+            break;
+        }  
+    }
+
+   next();
+}
+
 module.exports = {
     checkUpload,
-    renameUpload
+    renameUpload,
+    findPath
 }
