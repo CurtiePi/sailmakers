7f6ff8ed51838cf7873bc9b247df9d6c2d3bd273
CurtiePi <curtisbklyn@gmail.com>
2021-10-06 10:47:38 -0400

    corrected the phone number in the signature

--- config/signature.js
+++ config/signature.js
@@ -2,7 +2,7 @@
 const glyph = '~~_/)~~';
 const name = '<br/><span style="font-family: Arial; color: #436663; font-size: 1.75em">Capt. David Martin</span>';
 const email = '<a href="mailto:dave@uksailmakers-ne.com">dave@uksailmakers-ne.com</a>';
-const phone = '646-48305898';
+const phone = '646-4835898';
 const site = '<a href="www.uksailmakers-ne.com">www.uksailmakers-ne.com</a>';
 const embedded = '<img src="cid:dave@uk-sailmakers-ne.com"/>'
 
